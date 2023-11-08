5398869d7d291930d06e836ff23ef04c9d5646b6
poduran <poduran@gmail.com>
2023-11-08 14:32:17 -0300

    fix for nav and home from last commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:desktop_drop/desktop_drop.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:simple_intl_manager/models/project.dart';
+import 'package:simple_intl_manager/pages/home_loded.dart';
 import 'package:simple_intl_manager/widgets/nav.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,39 +17,42 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-            body: Row(children: [
-      const Nav(proj: null),
-      Expanded(
-          child: DropTarget(
-        onDragDone: ((details) {
-          File f = File(details.files.single.path);
-        }),
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton.icon(
-                  onPressed: () {
-                    newproject();
-                  },
-                  icon: const Icon(Icons.create_new_folder_outlined),
-                  label: const Text('New Project')),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton.icon(
-                  onPressed: () {
-                    openProject();
-                  },
-                  icon: const Icon(Icons.folder_open_outlined),
-                  label: const Text('Open Project')),
-            )
-          ]),
-        ]),
-      ))
-    ])));
+    return Nav(
+        proj: null,
+        body: DropTarget(
+            onDragDone: ((details) async {
+              File f = File(details.files.single.path);
+
+              Project proj = projectFromJson(await f.readAsString());
+
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => HomepageLoaded(proj: proj)));
+            }),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton.icon(
+                      onPressed: () {
+                        newproject();
+                      },
+                      icon: const Icon(Icons.create_new_folder_outlined),
+                      label: const Text('New Project')),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton.icon(
+                      onPressed: () {
+                        openProject();
+                      },
+                      icon: const Icon(Icons.folder_open_outlined),
+                      label: const Text('Open Project')),
+                )
+              ]),
+            ])));
   }
 
   openProject() async {
@@ -56,6 +61,11 @@ class _HomePageState extends State<HomePage> {
 
     if (res != null) {
       File f = File(res.files.first.path!);
+
+      Project proj = projectFromJson(await f.readAsString());
+
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => HomepageLoaded(proj: proj)));
     }
   }
 
--- lib/widgets/nav.dart
+++ lib/widgets/nav.dart
@@ -1,12 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:simple_intl_manager/models/project.dart';
+import 'package:simple_intl_manager/pages/home.dart';
 import 'package:simple_intl_manager/providers/special_themes.dart';
 
 class Nav extends StatefulWidget {
-  const Nav({super.key, required this.proj});
+  const Nav({super.key, required this.proj, required this.body});
 
   final Project? proj;
+  final Widget body;
 
   @override
   State<Nav> createState() => _NavState();
@@ -14,56 +17,90 @@ class Nav extends StatefulWidget {
 
 class _NavState extends State<Nav> {
   bool isLight = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Theme.of(context).appBarTheme.backgroundColor,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Tooltip(
-            message: 'Home page',
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                  onPressed: () {}, child: const Icon(Icons.home_outlined)),
+    bool isLoaded = widget.proj != null;
+
+    return SafeArea(
+      child: Scaffold(
+        body: Row(
+          children: [
+            Container(
+              color: Theme.of(context).appBarTheme.backgroundColor,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Tooltip(
+                    message: 'Home page',
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                          onPressed: isLoaded
+                              ? null
+                              : () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => HomePage()));
+                                },
+                          child: const Icon(Icons.home_outlined)),
+                    ),
+                  ),
+                  Tooltip(
+                    message: 'Configurations',
+                    child: Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: ElevatedButton(
+                            onPressed: isLoaded ? () {} : null,
+                            child: const Icon(Icons.settings_outlined))),
+                  ),
+                  Tooltip(
+                    message: 'Translations',
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                          onPressed: isLoaded ? () {} : null,
+                          child: const Icon(Icons.text_fields_outlined)),
+                    ),
+                  ),
+                  Expanded(child: Container()),
+                  kDebugMode
+                      ? Padding(
+                          padding: EdgeInsets.all(8),
+                          child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => HomePage()));
+                              },
+                              child: Icon(Icons.home)))
+                      : Container(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Switch(
+                          value: isLight,
+                          onChanged: ((value) {
+                            final provider = Provider.of<ThemeProvider>(context,
+                                listen: false);
+                            setState(() {
+                              isLight = !isLight;
+                            });
+                            isLight
+                                ? provider.setLightMode()
+                                : provider.setDarkMode();
+                          })),
+                      Icon(isLight ? Icons.dark_mode : Icons.light_mode)
+                    ],
+                  )
+                ],
+              ),
             ),
-          ),
-          Tooltip(
-            message: 'Configurations',
-            child: Padding(
-                padding: const EdgeInsets.all(8),
-                child: ElevatedButton(
-                    onPressed: () {},
-                    child: const Icon(Icons.settings_outlined))),
-          ),
-          Tooltip(
-            message: 'Translations',
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Icon(Icons.text_fields_outlined)),
-            ),
-          ),
-          Expanded(child: Container()),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Switch(
-                  value: isLight,
-                  onChanged: ((value) {
-                    final provider =
-                        Provider.of<ThemeProvider>(context, listen: false);
-                    setState(() {
-                      isLight = !isLight;
-                    });
-                    isLight ? provider.setLightMode() : provider.setDarkMode();
-                  })),
-              Icon(isLight ? Icons.dark_mode : Icons.light_mode)
-            ],
-          )
-        ],
+            Expanded(child: widget.body)
+          ],
+        ),
       ),
     );
   }
